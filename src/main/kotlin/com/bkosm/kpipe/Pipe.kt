c1165ebf07ac0ef07bcb4d8feb14e36a84742a13
bkosm <bartoshmen@gmail.com>
2022-11-28 15:08:53 +0100

    format; force no hint about moving last lambda out of scope

--- src/main/kotlin/com/bkosm/kpipe/Pipe.kt
+++ src/main/kotlin/com/bkosm/kpipe/Pipe.kt
@@ -2,16 +2,19 @@ package com.bkosm.kpipe
 
 
 class Pipe {
+    @Suppress("UNUSED_PARAMETER")
     companion object {
         inline operator fun <START, S1> invoke(
             initial: START,
-            step1: (START) -> S1
+            step1: (START) -> S1,
+            hintStop: Unit = Unit
         ) = step1(initial)
 
         inline operator fun <START, S1, S2> invoke(
             initial: START,
             step1: (START) -> S1,
             step2: (S1) -> S2,
+            hintStop: Unit = Unit
         ) = step2(step1(initial))
 
         inline operator fun <START, S1, S2, S3> invoke(
@@ -19,6 +22,7 @@ class Pipe {
             step1: (START) -> S1,
             step2: (S1) -> S2,
             step3: (S2) -> S3,
+            hintStop: Unit = Unit
         ) = step3(step2(step1(initial)))
 
         inline operator fun <START, S1, S2, S3, S4> invoke(
@@ -27,6 +31,7 @@ class Pipe {
             step2: (S1) -> S2,
             step3: (S2) -> S3,
             step4: (S3) -> S4,
+            hintStop: Unit = Unit
         ) = step4(step3(step2(step1(initial))))
 
         inline operator fun <START, S1, S2, S3, S4, S5> invoke(
@@ -36,6 +41,7 @@ class Pipe {
             step3: (S2) -> S3,
             step4: (S3) -> S4,
             step5: (S4) -> S5,
+            hintStop: Unit = Unit
         ) = step5(step4(step3(step2(step1(initial)))))
 
         inline operator fun <START, S1, S2, S3, S4, S5, S6> invoke(
@@ -46,6 +52,7 @@ class Pipe {
             step4: (S3) -> S4,
             step5: (S4) -> S5,
             step6: (S5) -> S6,
+            hintStop: Unit = Unit
         ) = step6(step5(step4(step3(step2(step1(initial))))))
 
         inline operator fun <START, S1, S2, S3, S4, S5, S6, S7> invoke(
@@ -57,6 +64,7 @@ class Pipe {
             step5: (S4) -> S5,
             step6: (S5) -> S6,
             step7: (S6) -> S7,
+            hintStop: Unit = Unit
         ) = step7(step6(step5(step4(step3(step2(step1(initial)))))))
 
         inline operator fun <START, S1, S2, S3, S4, S5, S6, S7, S8> invoke(
@@ -69,6 +77,7 @@ class Pipe {
             step6: (S5) -> S6,
             step7: (S6) -> S7,
             step8: (S7) -> S8,
+            hintStop: Unit = Unit
         ) = step8(step7(step6(step5(step4(step3(step2(step1(initial))))))))
 
         inline operator fun <START, S1, S2, S3, S4, S5, S6, S7, S8, S9> invoke(
@@ -82,6 +91,7 @@ class Pipe {
             step7: (S6) -> S7,
             step8: (S7) -> S8,
             step9: (S8) -> S9,
+            hintStop: Unit = Unit
         ) = step9(step8(step7(step6(step5(step4(step3(step2(step1(initial)))))))))
 
         inline operator fun <START, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10> invoke(
@@ -96,6 +106,7 @@ class Pipe {
             step8: (S7) -> S8,
             step9: (S8) -> S9,
             step10: (S9) -> S10,
+            hintStop: Unit = Unit
         ) = step10(step9(step8(step7(step6(step5(step4(step3(step2(step1(initial))))))))))
     }
 }
--- src/test/kotlin/com/bkosm/kpipe/PipeTest.kt
+++ src/test/kotlin/com/bkosm/kpipe/PipeTest.kt
@@ -8,8 +8,8 @@ internal class PipeTest {
     @Test
     fun `of 1`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
+            2,
+            { it * 3 },
         )
 
         expect(6) { res }
@@ -18,9 +18,9 @@ internal class PipeTest {
     @Test
     fun `of 2`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
+            2,
+            { it * 3 },
+            Int::toString,
         )
 
         expect("6") { res }
@@ -29,10 +29,10 @@ internal class PipeTest {
     @Test
     fun `of 3`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
         )
 
         expect(6.0) { res }
@@ -41,11 +41,11 @@ internal class PipeTest {
     @Test
     fun `of 4`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
         )
 
         expect(36.0) { res }
@@ -54,12 +54,12 @@ internal class PipeTest {
     @Test
     fun `of 5`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
         )
 
         expect("36.0") { res }
@@ -68,13 +68,13 @@ internal class PipeTest {
     @Test
     fun `of 6`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
-            step6 = { it.split("") },
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
+            { it.split("") },
         )
 
         expect(listOf("", "3", "6", ".", "0", "")) { res }
@@ -82,16 +82,14 @@ internal class PipeTest {
 
     @Test
     fun `of 7`() {
-        val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
-            step6 = { it.split("") },
-            step7 = { it.filter { c -> c.isNotBlank() } }
-        )
+        val res = Pipe(2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
+            { it.split("") },
+            { it.filter { c -> c.isNotBlank() } })
 
         expect(listOf("3", "6", ".", "0")) { res }
     }
@@ -99,15 +97,15 @@ internal class PipeTest {
     @Test
     fun `of 8`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
-            step6 = { it.split("") },
-            step7 = { it.filter { c -> c.isNotBlank() } },
-            step8 = { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
+            { it.split("") },
+            { it.filter { c -> c.isNotBlank() } },
+            { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
         )
 
         expect(listOf("33", "66", ".", "00")) { res }
@@ -116,16 +114,16 @@ internal class PipeTest {
     @Test
     fun `of 9`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
-            step6 = { it.split("") },
-            step7 = { it.filter { c -> c.isNotBlank() } },
-            step8 = { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
-            step9 = { it.joinToString("") },
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
+            { it.split("") },
+            { it.filter { c -> c.isNotBlank() } },
+            { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
+            { it.joinToString("") },
         )
 
         expect("3366.00") { res }
@@ -134,17 +132,17 @@ internal class PipeTest {
     @Test
     fun `of 10`() {
         val res = Pipe(
-            initial = 2,
-            step1 = { it * 3 },
-            step2 = Int::toString,
-            step3 = String::toDouble,
-            step4 = { it.pow(2) },
-            step5 = Double::toString,
-            step6 = { it.split("") },
-            step7 = { it.filter { c -> c.isNotBlank() } },
-            step8 = { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
-            step9 = { it.joinToString("") },
-            step10 = String::toDouble,
+            2,
+            { it * 3 },
+            Int::toString,
+            String::toDouble,
+            { it.pow(2) },
+            Double::toString,
+            { it.split("") },
+            { it.filter { c -> c.isNotBlank() } },
+            { it.map { c -> if (c.get(0) in '0'..'9') "$c$c" else c } },
+            { it.joinToString("") },
+            String::toDouble,
         )
 
         expect(3366.0) { res }
